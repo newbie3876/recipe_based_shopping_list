2f1915643e6f2ec5a841e739549c17d653d33c84
vidmantas <jvidmis@gmail.com>
2025-05-12 22:35:32 +0300

    Add IngredientService (Methods: findIngredientById, existsIngredientByName, saveIngredient and deleteIngredientById)

---
+++ praktinis_darbas/src/main/java/lt/techin/service/IngredientService.java
@@ -0,0 +1,35 @@
+package lt.techin.service;
+
+import lt.techin.model.Ingredient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class IngredientService {
+
+  private final IngredientRepository ingredientRepository;
+
+  @Autowired
+  public IngredientService(IngredientRepository ingredientRepository) {
+    this.ingredientRepository = ingredientRepository;
+  }
+
+  public Optional<Ingredient> findIngredientById(Long id) {
+    return this.ingredientRepository.findById(id);
+  }
+
+  public boolean existsIngredientByName(String name) {
+    return this.ingredientRepository.existsByName(name);
+  }
+
+  public Ingredient saveIngredient(Ingredient ingredient) {
+    return this.ingredientRepository.save(ingredient);
+  }
+
+  public void deleteIngredientById(Long id) {
+    this.ingredientRepository.deleteById(id);
+  }
+}
+
