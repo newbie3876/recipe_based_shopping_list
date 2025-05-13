2f81e1f822ddf772f17adb9223db221934fc7764
vidmantas <jvidmis@gmail.com>
2025-05-13 19:17:46 +0300

    Modified SecurityConfig (add requestMatchers GET("/api/ingredients") and GET("/api/ingredients/{id}"))

--- praktinis_darbas/src/main/java/lt/techin/security/SecurityConfig.java
+++ praktinis_darbas/src/main/java/lt/techin/security/SecurityConfig.java
@@ -53,6 +53,8 @@ public class SecurityConfig {
                     .requestMatchers(HttpMethod.PUT, "/api/users/{id}").authenticated()
                     .requestMatchers(HttpMethod.DELETE, "/api/users/{id}").authenticated()
 
+                    .requestMatchers(HttpMethod.GET, "/api/ingredients").hasRole("USER")
+                    .requestMatchers(HttpMethod.GET, "/api/ingredients/{id}").hasRole("USER")
                     .requestMatchers(HttpMethod.POST, "/api/ingredients").hasRole("USER")
                     .requestMatchers(HttpMethod.DELETE, "/api//ingredients/{id}").hasRole("USER")
                     .anyRequest().authenticated()
