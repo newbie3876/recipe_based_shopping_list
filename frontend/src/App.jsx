e882d0cf565f6d15dee656cbb32258cac483705c
Kostas <k.stelmokas37@gmail.com>
2025-05-13 18:51:01 +0300

    Pataisyta

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,6 @@ function App() {
 
   return (
     <main>
-      <NavBar />
       <div className="App">
         <Header />
         <Routes>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,18 +1,33 @@
-import React from "react";
 import { useAuth } from "../context/AuthContext";
+import { Link } from "react-router-dom";
 
 function Header() {
   const { logout, isAuthenticated } = useAuth();
 
   return (
-    <header className="p-4 bg-gray-200 flex justify-between">
+    <header className="p-4 bg-gray-200 flex justify-evenly">
       <h1>My App</h1>
       {isAuthenticated ? (
         <div>
-      
-          <button onClick={logout} className="text-red-500">
-            Atsijungti
-          </button>
+
+        <nav className="flex justify-evenly gap-[1rem]">
+          <Link to="/">
+          <button>Į pradinį puslapį</button>
+          </Link>
+          <Link to="/recipes">
+              <button>Į receptų puslapį</button>
+          </Link>
+          <Link to="/shoppinglists">
+            <button>Į pirkinių krepšelių puslapį</button>
+          </Link>
+          <Link to="/reciperegistration">
+            <button>Į receptų registracijos formą</button>
+          </Link>
+          </nav>
+        
+            <button onClick={logout} className="text-red-500">
+              Atsijungti
+            </button>
         </div>
       ) : (
         <p className="text-green-500">Neprisijungęs</p>
