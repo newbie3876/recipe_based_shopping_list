d426ff6a74a03ef0f7f8e055394c031303ecf5e5
vidmantas <jvidmis@gmail.com>
2025-05-14 14:08:10 +0300

    Add IngredientCategoryService with method getCategoryById and modified IngredientCategoryRepository interface

--- praktinis_darbas/src/main/java/lt/techin/repository/IngredientCategoryRepository.java
+++ praktinis_darbas/src/main/java/lt/techin/repository/IngredientCategoryRepository.java
@@ -8,5 +8,8 @@ import java.util.Optional;
 
 @Repository
 public interface IngredientCategoryRepository extends JpaRepository<IngredientCategory, Long> {
+
   Optional<IngredientCategory> findByName(String name);
+
+  Optional<IngredientCategory> getCategoryById(Long id);
 }
