3b523871401e3bc33861059d4491dfb7a2165900
[Migle] <[migle.adomaviciute@stud.techin.lt]>
2025-05-22 18:50:06 +0300

    Resolve conflicts ShoppingList

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/recipes" element={<RecipePage />} />
-          <Route path="/shoppinglists" element={<ShoppingList />} />
+          <Route path="/shoppinglists" element={<ShoppingListPage />} />
           <Route path="/reciperegistration" element={<AddRecipeForm />} />
           <Route path="/photoalbum" element={<PhotoAlbum />} />
         </Routes>
