32b0584a693d2bed7a7cc1840ec7deaffdc885a7
vidmantas <jvidmis@gmail.com>
2025-05-29 20:20:46 +0300

    Deleted unnecessary components.

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,5 +1,4 @@
 import { useNavigate } from "react-router-dom";
-import IngredientBox from "./IngredientBox";
 
 export default function Homepage() {
   const navigate = useNavigate();
@@ -23,58 +22,7 @@ export default function Homepage() {
             Rodyti visus ingredientus
           </button>
         </div>
-        {/* <div style={{ border: "1px solid gray", padding: "15px", boxShadow: "3px 3px 12px rgba(0, 0, 0, 0.3)", borderRadius: "8px", textAlign: "center" }}>
-          <button 
-            onClick={() => navigate("/add-ingredient")}
-            style={{
-              padding: "10px",
-              borderRadius: "5px",
-              backgroundColor: "#4CAF50",
-              color: "white",
-              border: "none",
-              cursor: "pointer",
-              boxShadow: "2px 2px 8px rgba(0, 0, 0, 0.2)"
-            }}
-          >
-            Pridėti naują ingredientą
-          </button>
-        </div> */}
       </div>
-      {/* <div style={{ display: "flex", gap: "20px", flexWrap: "wrap", justifyContent: "center" }}>
-        <div style={{ border: "1px solid gray", padding: "15px", boxShadow: "3px 3px 12px rgba(0, 0, 0, 0.3)", borderRadius: "8px", textAlign: "center" }}>
-          <button 
-            onClick={() => navigate("/")}
-            style={{
-              padding: "10px",
-              borderRadius: "5px",
-              backgroundColor: "#4CAF50",
-              color: "white",
-              border: "none",
-              cursor: "pointer",
-              boxShadow: "2px 2px 8px rgba(0, 0, 0, 0.2)"
-            }}
-          >
-            Peržiūrėti visus pirkinių sąrašus
-          </button>
-        </div> */}
-        {/* <div style={{ border: "1px solid gray", padding: "15px", boxShadow: "3px 3px 12px rgba(0, 0, 0, 0.3)", borderRadius: "8px", textAlign: "center" }}>
-          <button 
-            onClick={() => navigate("/create-shoppinglists")}
-            style={{
-              padding: "10px",
-              borderRadius: "5px",
-              backgroundColor: "#4CAF50",
-              color: "white",
-              border: "none",
-              cursor: "pointer",
-              boxShadow: "2px 2px 8px rgba(0, 0, 0, 0.2)"
-            }}
-          >
-            Sukurti naują pirkinių krepšelį
-          </button>
-        </div>
-      </div> */}
     </div>
-    
   );
 }
